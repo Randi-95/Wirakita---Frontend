9729fa24a642a961a918cd8da3a80a88dd6df2ca
Randi-95 <randiganteng9528@gmail.com>
2025-09-16 06:56:10 +0700

    update foto kategori serba alat

--- src/view/SerbaAlat.vue
+++ src/view/SerbaAlat.vue
@@ -10,25 +10,25 @@
         <div class="lg:grid lg:grid-cols-[1fr_4fr] mt-4">
             <div class="lg:flex flex-col hidden">
                 <div class="pr-8 flex flex-col gap-6">
-                    <div class="flex gap-2 items-center w-full justify-between h-8 border-l-2 border-[#06CAF5]">
+                    <div class="flex cursor-pointer gap-2 items-center w-full justify-between h-8 border-l-2 border-[#06CAF5]">
                         <h2 class="text-gray-800 text-sm ml-2">Filter Harga</h2>
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
                         <polyline points="18 15 12 9 6 15"></polyline>
                         </svg>
                     </div>
-                    <div class="flex gap-2 items-center w-full justify-between h-8 border-l-2 border-[#06CAF5]">
+                    <div class="flex cursor-pointer gap-2 items-center w-full justify-between h-8 border-l-2 border-[#06CAF5]">
                         <h2 class="text-gray-800 text-sm ml-2">Filter Kelas</h2>
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
                         <polyline points="18 15 12 9 6 15"></polyline>
                         </svg>
                     </div>
-                    <div class="flex gap-2 items-center w-full justify-between h-8 border-l-2 border-[#06CAF5]">
+                    <div class="flex cursor-pointer gap-2 items-center w-full justify-between h-8 border-l-2 border-[#06CAF5]">
                         <h2 class="text-gray-800 text-sm ml-2">Tipe Transaksi</h2>
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
                         <polyline points="18 15 12 9 6 15"></polyline>
                         </svg>
                     </div>
-                    <div class="flex gap-2 items-center w-full justify-between h-8 border-l-2 border-[#06CAF5]">
+                    <div class="flex cursor-pointer gap-2 items-center w-full justify-between h-8 border-l-2 border-[#06CAF5]">
                         <h2 class="text-gray-800 text-sm ml-2">Terakhir Ditambahkan</h2>
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
                         <polyline points="18 15 12 9 6 15"></polyline>
@@ -41,7 +41,7 @@
                     <div class="container-scroll grid grid-cols-2 lg:grid-cols-4 gap-x-4 my-4 gap-y-4 overflow-x-auto">
                     <div class="lg:w-50 w-auto flex flex-col items-center flex-shrink-0">
                         <div class="">                
-                            <img src="/public/image 7.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
+                            <img src="/public/contohfoto.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
                         </div>
                         <div class="">
                             <h2 class="lg:text-md text-sm font-medium leading-[20px] mt-2">Ayam Tepung Roti Spesial</h2>
@@ -51,18 +51,7 @@
                     </div>
                     <div class="lg:w-50 w-auto flex flex-col items-center flex-shrink-0">
                         <div class="">                
-                            <img src="/public/image 7.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
-                        </div>
-                        <div class="">
-                            <h2 class="lg:text-md text-sm font-medium leading-[20px] mt-2">Ayam Tepung Roti Spesial</h2>
-                            <h2 class="font-bold">Rp. 10.000</h2>
-                            <p class="text-xs lg:text-sm font-md text-gray-500">⭐5.0 | Terjual 1239</p>
-                        </div>
-                    </div>
-
-                    <div class="lg:w-50 w-auto flex flex-col items-center flex-shrink-0">
-                        <div class="">                
-                            <img src="/public/image 7.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
+                            <img src="/public/contohfoto.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
                         </div>
                         <div class="">
                             <h2 class="lg:text-md text-sm font-medium leading-[20px] mt-2">Ayam Tepung Roti Spesial</h2>
@@ -73,7 +62,7 @@
 
                     <div class="lg:w-50 w-auto flex flex-col items-center flex-shrink-0">
                         <div class="">                
-                            <img src="/public/image 7.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
+                            <img src="/public/contohfoto.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
                         </div>
                         <div class="">
                             <h2 class="lg:text-md text-sm font-medium leading-[20px] mt-2">Ayam Tepung Roti Spesial</h2>
@@ -84,7 +73,7 @@
 
                     <div class="lg:w-50 w-auto flex flex-col items-center flex-shrink-0">
                         <div class="">                
-                            <img src="/public/image 7.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
+                            <img src="/public/contohfoto.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
                         </div>
                         <div class="">
                             <h2 class="lg:text-md text-sm font-medium leading-[20px] mt-2">Ayam Tepung Roti Spesial</h2>
@@ -95,7 +84,7 @@
 
                     <div class="lg:w-50 w-auto flex flex-col items-center flex-shrink-0">
                         <div class="">                
-                            <img src="/public/image 7.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
+                            <img src="/public/contohfoto.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
                         </div>
                         <div class="">
                             <h2 class="lg:text-md text-sm font-medium leading-[20px] mt-2">Ayam Tepung Roti Spesial</h2>
@@ -106,7 +95,7 @@
 
                     <div class="lg:w-50 w-auto flex flex-col items-center flex-shrink-0">
                         <div class="">                
-                            <img src="/public/image 7.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
+                            <img src="/public/contohfoto.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
                         </div>
                         <div class="">
                             <h2 class="lg:text-md text-sm font-medium leading-[20px] mt-2">Ayam Tepung Roti Spesial</h2>
@@ -117,7 +106,7 @@
 
                     <div class="lg:w-50 w-auto flex flex-col items-center flex-shrink-0">
                         <div class="">                
-                            <img src="/public/image 7.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
+                            <img src="/public/contohfoto.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
                         </div>
                         <div class="">
                             <h2 class="lg:text-md text-sm font-medium leading-[20px] mt-2">Ayam Tepung Roti Spesial</h2>
@@ -128,7 +117,18 @@
 
                     <div class="lg:w-50 w-auto flex flex-col items-center flex-shrink-0">
                         <div class="">                
-                            <img src="/public/image 7.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
+                            <img src="/public/contohfoto.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
+                        </div>
+                        <div class="">
+                            <h2 class="lg:text-md text-sm font-medium leading-[20px] mt-2">Ayam Tepung Roti Spesial</h2>
+                            <h2 class="font-bold">Rp. 10.000</h2>
+                            <p class="text-xs lg:text-sm font-md text-gray-500">⭐5.0 | Terjual 1239</p>
+                        </div>
+                    </div>
+
+                    <div class="lg:w-50 w-auto flex flex-col items-center flex-shrink-0">
+                        <div class="">                
+                            <img src="/public/contohfoto.png" alt="" class="lg:w-50 w-auto rounded-lg"></img>
                         </div>
                         <div class="">
                             <h2 class="lg:text-md text-sm font-medium leading-[20px] mt-2">Ayam Tepung Roti Spesial</h2>
